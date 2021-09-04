cba0db15d90a54c071b2ed5b834161a93679d89f
nizam <nizamstudymajliz@gmail.com>
2021-09-04 20:26:55 +0530

    Services added

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Title from "../../Components/Home/Title";
+import "./Services.scss";
+
 
 function Services() {
     return (
-        <div>
-            
+        <div className="services">
+            <div className="services-title">
+            <Title
+          heading1="My awsome"
+          heading2="Services"
+          paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit.dgfhhfhfd sint eum ut,deserunt"
+          button_name="Download cv"
+        />
+            </div>
+            <div className="services-box">
+                 <img src="" alt="design" />
+                 <h3>Design</h3>
+            </div>
+            <div className="services-box2">
+
+            </div>
+            <div className="services-box3">
+
+            </div>
+           
         </div>
     )
 }
