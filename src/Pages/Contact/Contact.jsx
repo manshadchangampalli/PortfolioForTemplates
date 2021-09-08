9f8dbeedcd10af463eeb55ffdf451a54ba8f6494
manshad <manshadchangampalli010@gmail.com>
2021-09-08 17:06:54 +0530

    the title structure of the contact is finished

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import './Contact.scss'
+import React from "react";
+import "./Contact.scss";
 
 function Contact() {
-    return (
-        <div className="contact">
-            <div className="contact-title">
-                <h1>Contact Us</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="contact">
+      <div className="contact-title">
+        <h1>
+          Contact Us
+          <br />
+          Ways To Contact me
+        </h1>
+        <p>
+          In publishing and graphic design, Lorem ipsum is a placeholder text
+          commonly used to demonstrate the visual form of a document typeface
+          without relying on meaningful content.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
